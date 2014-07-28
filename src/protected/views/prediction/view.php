11d8e001b8d0260ed6d16c85cb00f91aed3f32d4
tinchopavl <tinchopavl@users.noreply.github.com>
2014-07-28 10:41:23 -0300

    Update view.php

--- src/protected/views/prediction/view.php
+++ src/protected/views/prediction/view.php
@@ -15,28 +15,36 @@ $this->menu=array(
 	array('label'=>'Manage Prediction', 'url'=>array('admin')),
 );
 ?>
+<div class="panel panel-primary">
+	<div class="panel-heading">
+		<h3 class="panel-title">
+		<span class="badge"><?php echo isset($model->localGoals) ? $model->localGoals : '-'; ?></span>&nbsp;
+		<?php echo $model->matchRel->localTeam0->name;?>
+		&nbsp;-&nbsp;<?php echo $model->matchRel->visitantTeam0->name;?>
+		<span class="badge"><?php echo isset($model->localGoals) ? $model->visitantGoals : '-'; ?></span>
+	</div>
+	<div class="panel-body">
+		<p>
+			<strong><?php echo isset($model->ques1) ? $model->ques1->text : ''; ?></strong>
+			<?php echo isset($model->answer1) ? $model->ques1->getOption($model->answer1) : ''; ?>
+		</p>
+		<p>
+			<strong><?php echo isset($model->ques2) ? $model->ques2->text : ''; ?></strong>
+			<?php echo isset($model->answer2) ? $model->ques2->getOption($model->answer2) : ''; ?>
+		</p>
+		<p>
+			<strong><?php echo isset($model->ques3) ? $model->ques3->text : ''; ?></strong>
+			<?php echo isset($model->answer3) ? $model->ques3->getOption($model->answer3) : ''; ?>
+		</p>
+		<p>
+			<strong><?php echo isset($model->ques4) ? $model->ques4->text : ''; ?></strong>
+			<?php echo isset($model->answer4) ? $model->ques4->getOption($model->answer4) : ''; ?>
+		</p>
+		<p>
+			<strong><?php echo isset($model->ques5) ? $model->ques5->text : ''; ?></strong>
+			<?php echo isset($model->answer5) ? $model->ques5->getOption($model->answer5) : ''; ?>
+		</p>
+		<p><span class="label label-success"><strong>Puntos:&nbsp;</strong><?php echo $model->score; ?></span></p>
+	</div>
+</div>
 
-<h1>View Prediction #<?php echo $model->idPrediction; ?></h1>
-
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'idPrediction',
-		'localGoals',
-		'visitantGoals',
-		'score',
-		'question1',
-		'answer1',
-		'question2',
-		'answer2',
-		'question3',
-		'answer3',
-		'question4',
-		'answer4',
-		'question5',
-		'answer5',
-		'user',
-		'match',
-		'predictioncol',
-	),
-)); ?>
