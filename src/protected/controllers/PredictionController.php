bc9f61979c89bb7cfa6e28a069f0a9aeda7995f4
Miguel <miguellatuf@gmail.com>
2014-07-31 21:37:25 -0300

    Rules, (revisar)!!!

--- src/protected/controllers/PredictionController.php
+++ src/protected/controllers/PredictionController.php
@@ -32,13 +32,13 @@ class PredictionController extends Controller
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update', 'admin', 'delete'),
+				'actions'=>array('create','update', 'delete'),
 				'users'=>array('@'),
 			),
-			/*array('allow', // allow admin user to perform 'admin' and 'delete' actions
+			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
 				'users'=>array('admin'),
-			),*/
+			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
 			),
