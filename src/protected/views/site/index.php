59c77504f62375bed95455a2089b2b0d093a0209
tinchopavl <tinchopav@gmail.com>
2014-08-04 14:43:45 -0300

    nuevo texto de la home

--- src/protected/views/site/index.php
+++ src/protected/views/site/index.php
@@ -5,7 +5,7 @@ $this->pageTitle=Yii::app()->name;
 ?>
 <div class="jumbotron text-center">
   <h1>Bienvenido a <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-  <p>blah blah blah blah</p>  
+  <p>El lugar que podr&aacute;s demostrar tus conocimientos futboleros y porque no tu suerte, pronosticando los resultados fecha a fecha de los torneos nacionales. La pelota ya est&aacute; en juego, segu&iacute; tu instinto futbolero y jug&aacute;.</p>  
   <p>
   	<?php echo CHtml::link('Registrarse', array('user/create'), array('class' => 'btn btn-primary btn-lg btn-register')); ?>&nbsp;
   	<?php echo CHtml::link('Iniciar Sesi&oacute;n', array('site/login'), array('class' => 'btn btn-default btn-lg')); ?>
