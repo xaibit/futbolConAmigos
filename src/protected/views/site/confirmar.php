c722c29220a945fc6ba20540e1dec13cd2c90107
tinchopavl <tinchopav@gmail.com>
2014-08-11 10:10:33 -0300

    cambio de texto

--- src/protected/views/site/confirmar.php
+++ src/protected/views/site/confirmar.php
@@ -5,7 +5,7 @@ $this->pageTitle=Yii::app()->name;
 ?>
 <div class="jumbotron text-center">
 	<p>Se ha enviado un correo a su cuenta para que podamos verificarla.
-		Por favor revise su correo electr&oacute;nico y confirme su correo.</p>
+		Por favor revise su correo electr&oacute;nico y confirme su correo. <strong>En caso de no encontrarlo, revise la carpeta SPAM.</strong></p>
 	<p>
   	<?php echo CHtml::link('Volver a Inicio', array('site/index'), array('class' => 'btn btn-primary btn-lg')); ?>  	
   </p>
